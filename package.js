9948139e995f5169d08ea212dd60a750aad54fba
MaazAli <maaz_ali@outlook.com>
2015-03-11 23:30:11 -0400

    Updated highcharts to v4.1.4 and cleaned up some other code

--- package.js
+++ package.js
@@ -1,21 +1,23 @@
 Package.describe({
     summary: "HighCharts for Meteor, with an easy to use helper to get you started!",
-    version: "0.2.3",
+    version: "0.2.4",
     git: "https://github.com/MaazAli/Meteor-HighCharts.git"
 });
 
 Package.onUse(function (api) {
-
-    api.versionsFrom('METEOR@0.9.1.1');
+    api.versionsFrom('METEOR@1.0.3.2');
     api.use('jquery');
     api.use('templating');
 
-    // basic highcharts 
-    api.addFiles('lib/highcharts.js', 'client');
-    // extra types
-    api.addFiles('lib/highcharts-more.js', 'client');
-    api.addFiles('lib/highcharts-solide-gauge.js', 'client');
-
-    api.addFiles('lib/highchartsHelper.html', 'client');
-    api.addFiles('lib/highchartsHelper.js', 'client');
+    // basic highcharts
+    api.addFiles([
+      // Core
+      'lib/highcharts.js',
+      // Extra types
+      'lib/highcharts-more.js',
+      'lib/highcharts-solide-gauge.js',
+      // Helper
+      'lib/highchartsHelper.html',
+      'lib/highchartsHelper.js'
+    ], 'client');
 });
