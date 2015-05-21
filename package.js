19e51957b5ca0f64d63e4383811c3278b1e0d5bc
jhuenges <huenges.j@googlemail.com>
2015-05-21 12:26:10 +0200

    Updated readme; added name to packages.js

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
     summary: "HighCharts for Meteor, with an easy to use helper to get you started!",
     version: "0.3.0",
-    git: "https://github.com/MaazAli/Meteor-HighCharts.git"
+    git: "https://github.com/MaazAli/Meteor-HighCharts.git",
+    name: "maazalik:highcharts"
 });
 
 Package.onUse(function (api) {
